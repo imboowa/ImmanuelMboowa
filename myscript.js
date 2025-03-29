1d142f7b4ebcc82b0a1bf5f3893a34f6a53c8fb0
Immanuel Mboowa <154756973+imboowa@users.noreply.github.com>
2025-03-29 08:06:16 -0400

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -8,11 +8,14 @@ function doSomething(){
         toggle.textContent = '🌙';
         switchOn = 0;
         document.body.style.transition = "1.0s ease-in";
+        document.querySelector(".codings").style.boxShadow = 'none';
     } else if (switchOn === 0) {
         document.body.style.backgroundColor = 'black';
         toggle.textContent = "☀️";
         switchOn = 1;
         document.body.style.transition = "1.0s ease-in";
+        document.querySelector(".codings").style.boxShadow = '0 0 50px darkorange';
+        document.querySelector(".codings").style.transition = '2.0s ease-in';
     }
 }
 toggle.addEventListener('click', doSomething);
@@ -27,3 +30,30 @@ function updateBar() {
     document.getElementById('progressBar').style.width = scrollPercentage + "%";
 }
 // background: linear-gradient(180deg, greenyellow, yellowgreen);
+// make all codings glitter
+let doOrange = setInterval(() => {
+    let desiredClass = document.querySelector(".codings");
+    if (desiredClass) {
+        desiredClass.style.borderRadius = '50px';
+        desiredClass.classList.add('nice');
+        clearInterval(doOrange);
+    }
+}, 500);
+
+window.onload = function() {
+    const letters = document.querySelectorAll("#myName span");
+    // nodeList animating every inline block char
+    letters.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.add('jump');
+            letter.style.color = 'black';
+            letter.classList.transition = '3.0s ease-in';
+        }, index * 600);
+    });
+    // remove all letters from classList jump
+    setTimeout(() => {
+        letters.forEach(letter => {
+            letter.classList.remove('jump');
+        });
+    }, 400); // in 400 seconds
+};
