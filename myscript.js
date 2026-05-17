7d3e458343ac8f5118f26ab6d81909f34397998d
Mboowa Immanuel <154756973+emboowae@users.noreply.github.com>
2026-05-17 10:30:03 -0400

    Commit 1

--- myscript.js
+++ myscript.js
@@ -1,59 +1,15 @@
-// Changes mode theme
-const toggle = document.getElementById('toggle');
-document.body.style.backgroundColor = 'black';
-switchOn = 1;
-function doSomething(){
-    if (switchOn === 1) {
-        document.body.style.backgroundColor = 'white';
-        toggle.textContent = '🌙';
-        switchOn = 0;
-        document.body.style.transition = "1.0s ease-in";
-        document.querySelector(".codings").style.boxShadow = 'none';
-    } else if (switchOn === 0) {
-        document.body.style.backgroundColor = 'black';
-        toggle.textContent = "☀️";
-        switchOn = 1;
-        document.body.style.transition = "1.0s ease-in";
-        document.querySelector(".codings").style.boxShadow = '0 0 50px darkorange';
-        document.querySelector(".codings").style.transition = '2.0s ease-in';
+function checkWindowSize()
+{
+    const width = window.innerWidth;
+    if (width < 835)
+    {
+        document.getElementById("for_the_header").style.display = "none";
+    } else
+    {
+        document.getElementById("for_the_header").style.display = "block";
     }
-}
-toggle.addEventListener('click', doSomething);
-// Updates scroll bar
-window.onscroll = function() {
-    updateBar();
-}
-function updateBar() {
-    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    let scrollPercentage = (scrollTop / scrollHeight) * 100;
-    document.getElementById('progressBar').style.width = scrollPercentage + "%";
-}
-// background: linear-gradient(180deg, greenyellow, yellowgreen);
-// make all codings glitter
-let doOrange = setInterval(() => {
-    let desiredClass = document.querySelector(".codings");
-    if (desiredClass) {
-        desiredClass.style.borderRadius = '50px';
-        desiredClass.classList.add('nice');
-        clearInterval(doOrange);
-    }
-}, 500);
-
-window.onload = function() {
-    const letters = document.querySelectorAll("#myName span");
-    // nodeList animating every inline block char
-    letters.forEach((letter, index) => {
-        setTimeout(() => {
-            letter.classList.add('jump');
-            letter.style.color = 'black';
-            letter.classList.transition = '3.0s ease-in';
-        }, index * 600);
-    });
-    // remove all letters from classList jump
-    setTimeout(() => {
-        letters.forEach(letter => {
-            letter.classList.remove('jump');
-        });
-    }, 400); // in 400 seconds
 };
+// On Window Reload
+checkWindowSize();
+// On Resizing Window
+window.addEventListener("resize", checkWindowSize);
