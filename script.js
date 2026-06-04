4e4e2bd84a2888b257ceb4e41f676b03d06ddc40
Mboowa Immanuel <154756973+emboowae@users.noreply.github.com>
2026-06-04 14:35:29 -0400

    Commit 4

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function checkWindowSize()
     if (width < 835)
     {
         document.getElementById("for_the_header").style.display = "none";
+        document.getElementById("contact_box").style.display = "flex";
     } else
     {
         document.getElementById("for_the_header").style.display = "block";
